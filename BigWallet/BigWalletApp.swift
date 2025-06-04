63f31451e2d7864b7df72ca2073fb2fdfeede551
Alfred Lotsu <alfredlotsu2@gmail.com>
2025-06-04 06:39:04 +0000

    Initial Commit

---
+++ BigWallet/BigWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigWalletApp.swift
+//  BigWallet
+//
+//  Created by Alfred Lotsu on 04/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct BigWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
